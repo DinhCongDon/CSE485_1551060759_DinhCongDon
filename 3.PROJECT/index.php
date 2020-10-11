fe201810a668c53fe0b68a9a37d6242bf9f95d2b
DinhCongDon <dinhdon190297@gmail.com>
2020-10-11 23:06:54 +0700

    donnn

---
+++ 3.PROJECT/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ban chủ nhiệm khoa Công nghệ thông tin</title>
+    <link rel="icon" type="image/jpg" href="images/icon.jpg">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="header-top full-container">
+            <div class="inner-container">
+                <div class="text-header-top">
+                    <a href="#">Trường Đại học Thủy Lợi -TLU</a>
+                </div> 
+                <div class="plus-header-top">
+                    <p>(+) </p>
+                </div> 
+                <div class="login-header-top">
+                    <a href="#">Login</a>
+                </div>
+                <div class="Register-header-top">
+                    <a href="#">Ngôn ngữ: </a>
+                    <div class="icon-lang">
+                        <a href="#"><img src="images/icon-lang-vi.png" alt=""></a>
+                        <a href="#"><img src="images/icon-lang-en.png" alt=""></a>  
+                    </div>
+                </div>
+                <div class="seach-header-top">
+                    <input type="text" placeholder="Tìm kiếm">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+    
+    </div>
+    <div class="footer">
+    </div>
+    
+    <script src="https://use.fontawesome.com/24b84051bd.js"></script>
+</body>
+</html>
